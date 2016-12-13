35485c54bb136d34db37f21aae54699ef3139adc
dn <gkvms963@naver.com>
2016-12-13 22:30:34 +0900

    Button클래스 내용 추가

--- src/Game/Object/Button.java
+++ src/Game/Object/Button.java
@@ -1,12 +1,27 @@
 package Game.Object;
 
 import Game.GameObject;
+import Graphics.Texture;
+import Graphics.VertexArray;
+import Maths.Matrix4f;
 
 public class Button implements GameObject{
+	private Texture m_Texture;
+	private VertexArray m_Vao;
+	private Matrix4f m_MVMatrix;
+	private float m_PosX;
+	private float m_PosY;
+	
+	public void Button() {
+		m_Texture = new Texture("");
+		m_Vao = new VertexArray(m_Texture.getWidth(), m_Texture.getHeight(), 0, 1, 0, 1);
+		m_MVMatrix = Matrix4f.translate(m_PosX, m_PosY, 0);
+	}
 
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
+		m_Texture.bind();
+		m_Vao.draw(m_MVMatrix);
 		
 	}
 
